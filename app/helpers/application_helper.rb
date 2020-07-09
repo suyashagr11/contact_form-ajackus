f83e04e9f1b9e6a838fef59b76984938a1b0d168
Suyash <suyashagrawal.1103@gmail.com>
2020-07-09 16:12:31 +0530

    Added controller, routes, view and assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_errors(object, field_name)
+    if object.errors.any?
+      if !object.errors.messages[field_name].blank?
+        field_name.to_s.camelize + ' ' + object.errors.messages[field_name].join(", ")
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get :contact, to: 'contacts#new', as: :contact
+  post :contact, to: 'contacts#create'
+
+  match '*path' => redirect('/contact'), via: :get
 end
